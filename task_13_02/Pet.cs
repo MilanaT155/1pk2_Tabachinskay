6ed53b7556c782baf48829e3df9561390bc2e589
MilanaT155 <m46867570@gmail.com>
2025-03-16 14:59:00 +0500

    Выполнены задачи 13 урока

---
+++ task_13_02/Pet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_13_02
+{
+    internal class Pet
+    {
+        public string name { set; get; }
+        public string animal { set; get; }
+        public int age { set; get; }
+        private int weight { set; get; }
+        private string health { set; get; }
+        public void GetWeight(int NewWeight)
+        {
+            
+            weight = NewWeight;
+        }
+        public void GetHealth(string NewHealth)
+        {
+           
+
+            health = NewHealth;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name} Вид животного: {animal} Возраст: {age} Вес: {weight} Отметка о состоянии здоровья: {health}");
+
+        }
+
+    }
+}
---
+++ task_13_03/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_13_03
+{
+    internal class Car
+    {
+        public string number { set; get; }
+        public string brand { set; get; }
+        public string color { set; get; }
+        public int speed { set; get; }
+        public void ride()
+        {
+            for (int i = 0; i < 30; i += 5)
+            {
+                speed += i;
+                if (speed > 60)
+                {
+                    Console.WriteLine("Скорость превыщена!!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Скрость равна: {speed} км/ч");
+                }
+            }
+
+
+        }
+        public void braking()
+        {
+            Console.WriteLine("Автомобиль остановлен");
+            speed = 0;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Номер автомобиля: {number}");
+            Console.WriteLine($"Бренд автомобиля: {brand}");
+            Console.WriteLine($"Цвет автомобиля: {color}");
+            Console.WriteLine($"Скорость автомобиля на данный момент: {speed} км/ч ");
+
+
+        }
+    }
+}
