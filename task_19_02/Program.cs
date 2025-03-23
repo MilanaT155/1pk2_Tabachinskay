f647a96f6bf4fc102fde089ed34b87da2f987165
MilanaT155 <m46867570@gmail.com>
2025-03-23 12:12:08 +0500

    Задачи 19 урока

---
+++ task_19_02/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task_19_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Напишите программу, которая запрашивает у пользователя произвольный текст, содержащий
+            //предложения(есть знаки препинания).программу после анализа выводит текст, разделенный на
+            //части:
+            //a)
+            //По пробелам(отдельные слова построчно)
+            //b)
+            //По предложениям(отдельные предложения построчно)
+            //(используйте метод Split())
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            string[] textArray = text.Split();
+            foreach (String s in textArray)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            Console.Write("Введите текст, сoстоящий из нескольких предложений: ");
+            string text2 = Console.ReadLine();
+            string[] text2Array = text2.Split(".");
+            foreach (String s in text2Array)
+            {
+                Console.WriteLine(s);
+            }
+
+
+        }
+    }
+}
---
+++ task_19_03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.Metrics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace task_19_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Напишите консольное приложение, в котором осуществляется построчный пользовательский
+            //ввод(ввод каждой строки подтверждается нажатием на enter).количество введенных строк
+            //произвольно, ввод завершается при вводе пустой строки.
+            //После окончания ввода произведите объединение всех ранеее введенных строк в одну с
+            //использованием разделителя «-»
+            //Например:
+            //            Введите строку 1: "Hello"
+            //Введите строку 2: "world"
+            //Введите строку 3: "C#"
+            //Результат: "Hello-world-C#"
+            string text;
+            string result= "";
+            Console.WriteLine("Введите строки: ");
+            while (true)
+            {
+                text = Console.ReadLine();
+                if (text == "")
+                {
+                    break;
+                }
+                else
+                {
+                    result += " ";
+
+                }
+                result += text;
+            }
+            text = String.Join("-", result.Split(' '));
+            Console.WriteLine($"Результат: {text} ");
+
+
+
+
+        }
+    }
+}
